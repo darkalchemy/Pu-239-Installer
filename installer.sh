1fb7bdb6238107d3c9ae5b3349ed2b726839bba0
darkalchemy <darkalchemy@jbmatrix.net>
2018-12-27 11:27:34 -0500

    fix: missing quotes

--- installer.sh
+++ installer.sh
@@ -327,18 +327,18 @@ ${GREEN}sudo crontab -e
 
 ${RED}### Use this if you DO NOT need any logging for these scripts
 ${GREEN}# runs cron_controller.php every minute, if not already running, as user www-data
-* * * * * su www-data -s /bin/bash -c "/usr/bin/php /var/www/${IPADDY}/include/cron_controller.php" >/dev/null 2>&1
+* * * * * su www-data -s /bin/bash -c \"/usr/bin/php /var/www/${IPADDY}/include/cron_controller.php\" >/dev/null 2>&1
 
 # this can take several minutes to run, especially the first time, so we run it separate
 # runs images_update.php every 30 minutes, if not already running, as user www-data
-*/30 * * * * su www-data -s /bin/bash -c "/usr/bin/php /var/www/${IPADDY}/include/images_update.php" >/dev/null 2>&1
+*/30 * * * * su www-data -s /bin/bash -c \"/usr/bin/php /var/www/${IPADDY}/include/images_update.php\" >/dev/null 2>&1
 
 ${RED}### Use this if you DO need any logging for these scripts
 ${GREEN}# runs cron_controller.php every minute, if not already running, as user www-data
-* * * * * su www-data -s /bin/bash -c "/usr/bin/php /var/www/${IPADDY}/include/cron_controller.php" >> /var/log/nginx/cron_`date +\%Y\%m\%d`.log 2>&1
+* * * * * su www-data -s /bin/bash -c \"/usr/bin/php /var/www/${IPADDY}/include/cron_controller.php\" >> /var/log/nginx/cron_`date +\%Y\%m\%d`.log 2>&1
 
 # this can take several minutes to run, especially the first time, so we run it separate
 # runs images_update.php every 30 minutes, if not already running, as user www-data
-*/30 * * * * su www-data -s /bin/bash -c "/usr/bin/php /var/www/${IPADDY}/include/images_update.php" >> /var/log/nginx/images_`date +\%Y\%m\%d`.log 2>&1
+*/30 * * * * su www-data -s /bin/bash -c \"/usr/bin/php /var/www/${IPADDY}/include/images_update.php\" >> /var/log/nginx/images_`date +\%Y\%m\%d`.log 2>&1
 $CLEAR"
 
