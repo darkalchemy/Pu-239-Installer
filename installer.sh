9231bdce29235e74e38887ab1801d86076ff7562
darkalchemy <darkalchemy@jbmatrix.net>
2019-01-11 17:54:00 -0500

    update: installer.sh

--- installer.sh
+++ installer.sh
@@ -2,11 +2,18 @@
 
 set -e
 #CONFIG - these must be set
-USERNAME=""         #username for mysql
-PASS=""             #password for mysql user
-DBNAME=""           #database name
-ROOTPASSWORD=""     # mysql root user password, this is needed to fix login by root user
-IPADDY=""           #fully qualified domain name or routable ip
+SITENAME=''         # the name that will be displayed throughout your site as the site name
+SITEHTTP=''         # fully qualified domain name, do not include http://
+SITESSL=''          # fully qualified domain name, do not include https://
+USERNAME=''         # username for mysql
+PASS=''             # password for mysql user
+DBNAME=''           # database name
+ROOTPASSWORD=''     # mysql root user password, this is needed to fix login by root user
+BOTNAME=''          # username for your site bot
+SITEEMAIL=''        # email that will be used by your site to send emails
+ADMINUSERNAME=''    # your first users username
+ADMINPASS=''        # your first users password
+ADMINEMAIL=''       # your first users email
 
 YELLOW="\033[1;33m"
 RED="\033[1;31m"
@@ -25,6 +32,21 @@ fi
 export USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
 usermod -a -G www-data $SUDO_USER
 usermod -a -G $SUDO_USER www-data
+
+if [[ $SITENAME == "" ]]; then
+    echo -e "${RED}You must fill in the site name"
+    exit
+fi
+
+if [[ $SITEHTTP == "" ]]; then
+    echo -e "${RED}You must fill in the FQDN non ssl"
+    exit
+fi
+
+if [[ $SITESSL == "" ]]; then
+    echo -e "${RED}You must fill in the FQDN ssl"
+    exit
+fi
 
 if [[ $USERNAME == "" ]]; then
     echo -e "${RED}You must fill in the username"
@@ -46,8 +68,28 @@ if [[ $ROOTPASSWORD == "" ]]; then
     exit
 fi
 
-if [[ $IPADDY == "" ]]; then
-    echo -e "${RED}You must fill in the ip address or FQDN"
+if [[ $BOTNAME == "" ]]; then
+    echo -e "${RED}You must fill in the sites bot username"
+    exit
+fi
+
+if [[ $SITEEMAIL == "" ]]; then
+    echo -e "${RED}You must fill in the sites email"
+    exit
+fi
+
+if [[ $ADMINUSERNAME == "" ]]; then
+    echo -e "${RED}You must fill in the admins username"
+    exit
+fi
+
+if [[ $ADMINPASS == "" ]]; then
+    echo -e "${RED}You must fill in the admins password"
+    exit
+fi
+
+if [[ $ADMINEMAIL == "" ]]; then
+    echo -e "${RED}You must fill in the admins email"
     exit
 fi
 
@@ -107,10 +149,10 @@ mkdir -p /var/log/nginx
 chmod 755 /var/log/nginx
 chown -R www-data:www-data /var/log/nginx
 wget --no-check-certificate https://raw.githubusercontent.com/darkalchemy/Pu-239-Installer/master/config/tracker -O /etc/nginx/sites-available/tracker
-sed -i "s/root.*$/root \/var\/www\/$IPADDY\/public\/;/" /etc/nginx/sites-available/tracker
+sed -i "s/root.*$/root \/var\/www\/$SITEHTTP\/public\/;/" /etc/nginx/sites-available/tracker
 rm -f /etc/nginx/sites-enabled/default
 ln -sf /etc/nginx/sites-available/tracker /etc/nginx/sites-enabled/
-sed -i "s/localhost/$IPADDY/" /etc/nginx/sites-available/tracker
+sed -i "s/localhost/$SITEHTTP/" /etc/nginx/sites-available/tracker
 wget --no-check-certificate https://raw.githubusercontent.com/darkalchemy/Pu-239-Installer/master/config/nginx.conf -O /etc/nginx/nginx.conf
 CORES=`cat /proc/cpuinfo | grep processor | wc -l`
 sed -i "s/^worker_processes.*$/worker_processes $CORES;/" /etc/nginx/nginx.conf
@@ -199,18 +241,18 @@ echo -e "${GREEN}Installed other, mostly needed, apps.$CLEAR"
 echo -e "${GREEN}Installed composer.$CLEAR"
 echo -e "${GREEN}Installed Node.js.$CLEAR"
 echo -e "${GREEN}Done.$CLEAR"
-echo -e "${YELLOW}Now we download the Pu-239 Source Code into /var/www/$IPADDY...\n\n$CLEAR"
+echo -e "${YELLOW}Now we download the Pu-239 Source Code into /var/www/$SITEHTTP...\n\n$CLEAR"
 cd /var/www/
-rm -fr /var/www/$IPADDY
-git clone https://github.com/darkalchemy/Pu-239.git $IPADDY
+rm -fr /var/www/$SITEHTTP
+git clone https://github.com/darkalchemy/Pu-239.git $SITEHTTP
 service mysql restart
 service php7.2-fpm restart
 service nginx restart
-cd /var/www/$IPADDY
-chown -R $SUDO_USER:www-data /var/www/$IPADDY
+cd /var/www/$SITEHTTP
+chown -R $SUDO_USER:www-data /var/www/$SITEHTTP
 $USER_HOME/bin/composer install
 sudo -u $SUDO_USER npm install
-chown -R www-data:www-data /var/www/$IPADDY
+chown -R www-data:www-data /var/www/$SITEHTTP
 
 clear
 echo -e "${GREEN}Installed PPA's.$CLEAR"
@@ -221,14 +263,10 @@ echo -e "${GREEN}Installed PHP, PHP-FPM.$CLEAR"
 echo -e "${GREEN}Installed other, mostly needed, apps.$CLEAR"
 echo -e "${GREEN}Installed composer.$CLEAR"
 echo -e "${GREEN}Installed Node.js.$CLEAR"
-echo -e "${GREEN}Downloaded the Pu-239 Source Code into /var/www/$IPADDY.$CLEAR"
+echo -e "${GREEN}Downloaded the Pu-239 Source Code into /var/www/$SITEHTTP.$CLEAR"
 echo -e "${GREEN}Done.$CLEAR"
-echo -e "${YELLOW}Now you need to point your browser to http://${IPADDY}/install/"
-echo -e "and complete the site installation process."
-echo -e "${RED}Please stop when you get here -> http://${IPADDY}/signup.php.$CLEAR"
-read -p "
-Once you have completed the above steps, press any key to continue:
-" -n 1 -r
+echo -e "${YELLOW}Installing your site.$CLEAR"
+php bin/install.php install "$SITENAME" "$SITEHTTP" "$SITESSL" "$DBNAME" "$USERNAME" "$PASS" "$BOTNAME" "$SITEEMAIL" "$ADMINUSERNAME" "$ADMINPASS" "$ADMINEMAIL"
 
 clear
 echo -e "${GREEN}Installed PPA's.$CLEAR"
@@ -239,29 +277,11 @@ echo -e "${GREEN}Installed PHP, PHP-FPM.$CLEAR"
 echo -e "${GREEN}Installed other, mostly needed, apps.$CLEAR"
 echo -e "${GREEN}Installed composer.$CLEAR"
 echo -e "${GREEN}Installed Node.js.$CLEAR"
-echo -e "${GREEN}Downloaded the Pu-239 Source Code into /var/www/$IPADDY.$CLEAR"
+echo -e "${GREEN}Downloaded the Pu-239 Source Code into /var/www/$SITEHTTP.$CLEAR"
 echo -e "${GREEN}Site installation completed.$CLEAR"
-echo -e "${GREEN}Done.$CLEAR"
-echo -e "${YELLOW}Importing trivia, tvmaze and images databases.$CLEAR"
-mysql $DBNAME < /var/www/$IPADDY/database/trivia.php.sql
-mysql $DBNAME < /var/www/$IPADDY/database/tvmaze.php.sql
-mysql $DBNAME < /var/www/$IPADDY/database/images.php.sql
-
-clear
-echo -e "${GREEN}Installed PPA's.$CLEAR"
-echo -e "${GREEN}Updated your system.$CLEAR"
-echo -e "${GREEN}Installed Percona XtraDB Server.$CLEAR"
-echo -e "${GREEN}Installed Nginx.$CLEAR"
-echo -e "${GREEN}Installed PHP, PHP-FPM.$CLEAR"
-echo -e "${GREEN}Installed other, mostly needed, apps.$CLEAR"
-echo -e "${GREEN}Installed composer.$CLEAR"
-echo -e "${GREEN}Installed Node.js.$CLEAR"
-echo -e "${GREEN}Downloaded the Pu-239 Source Code into /var/www/$IPADDY.$CLEAR"
-echo -e "${GREEN}Site installation completed.$CLEAR"
-echo -e "${GREEN}Imported trivia, tvmaze and images databases.$CLEAR"
 echo -e "${GREEN}Done.$CLEAR"
 echo -e "${YELLOW}Creating, merging, minifying and gzipping css and js files.$CLEAR"
-cd /var/www/$IPADDY
+cd /var/www/$SITEHTTP
 php bin/uglify.php
 
 clear
@@ -273,13 +293,12 @@ echo -e "${GREEN}Installed PHP, PHP-FPM.$CLEAR"
 echo -e "${GREEN}Installed other, mostly needed, apps.$CLEAR"
 echo -e "${GREEN}Installed composer.$CLEAR"
 echo -e "${GREEN}Installed Node.js.$CLEAR"
-echo -e "${GREEN}Downloaded the Pu-239 Source Code into /var/www/$IPADDY.$CLEAR"
+echo -e "${GREEN}Downloaded the Pu-239 Source Code into /var/www/$SITEHTTP.$CLEAR"
 echo -e "${GREEN}Site installation completed.$CLEAR"
-echo -e "${GREEN}Imported trivia, tvmaze and images databases.$CLEAR"
 echo -e "${GREEN}Created, merged, minified and gzipped css and js files.$CLEAR"
 echo -e "${GREEN}Done.$CLEAR"
 echo -e "${YELLOW}Setting correct permissions and ownership.$CLEAR"
-chown -R $SUDO_USER:www-data /var/www/$IPADDY
+chown -R $SUDO_USER:www-data /var/www/$SITEHTTP
 php bin/set_perms.php
 
 clear
@@ -291,54 +310,34 @@ echo -e "${GREEN}Installed PHP, PHP-FPM.$CLEAR"
 echo -e "${GREEN}Installed other, mostly needed, apps.$CLEAR"
 echo -e "${GREEN}Installed composer.$CLEAR"
 echo -e "${GREEN}Installed Node.js.$CLEAR"
-echo -e "${GREEN}Downloaded the Pu-239 Source Code into /var/www/$IPADDY.$CLEAR"
+echo -e "${GREEN}Downloaded the Pu-239 Source Code into /var/www/$SITEHTTP.$CLEAR"
 echo -e "${GREEN}Site installation completed.$CLEAR"
 echo -e "${GREEN}Imported trivia, tvmaze and images databases.$CLEAR"
 echo -e "${GREEN}Created, merged, minified and gzipped css and js files.$CLEAR"
 echo -e "${GREEN}Set correct permissions and ownership.$CLEAR"
-echo -e "${GREEN}Done.$CLEAR"
-echo -e "${YELLOW}Removing /var/www/$IPADDY/public/install.$CLEAR"
-rm -fr /var/www/$IPADDY/public/install
-
-clear
-echo -e "${GREEN}Installed PPA's.$CLEAR"
-echo -e "${GREEN}Updated your system.$CLEAR"
-echo -e "${GREEN}Installed Percona XtraDB Server.$CLEAR"
-echo -e "${GREEN}Installed Nginx.$CLEAR"
-echo -e "${GREEN}Installed PHP, PHP-FPM.$CLEAR"
-echo -e "${GREEN}Installed other, mostly needed, apps.$CLEAR"
-echo -e "${GREEN}Installed composer.$CLEAR"
-echo -e "${GREEN}Installed Node.js.$CLEAR"
-echo -e "${GREEN}Downloaded the Pu-239 Source Code into /var/www/$IPADDY.$CLEAR"
-echo -e "${GREEN}Site installation completed.$CLEAR"
-echo -e "${GREEN}Imported trivia, tvmaze and images databases.$CLEAR"
-echo -e "${GREEN}Created, merged, minified and gzipped css and js files.$CLEAR"
-echo -e "${GREEN}Set correct permissions and ownership.$CLEAR"
-echo -e "${GREEN}Removed /var/www/$IPADDY/public/install.$CLEAR"
 echo -e "${GREEN}Done.$CLEAR"
 
 clear
 echo -e "${GREEN}The installation of Pu-239 completed successfully.$CLEAR"
-echo -e "${YELLOW}Now, add yourself to the site by going to http://${IPADDY}/signup.php to create a new user.$CLEAR"
-echo -e "${YELLOW}Then, Follow the rest of the steps in the README:
+echo -e "${GREEN}The cleanup scripts require an addition to crontab as listed below:$CLEAR"
 
-${RED}# add cron job to root cron for running cleanup
+echo -e "${RED}# add cron job to root cron for running cleanup
 ${GREEN}sudo crontab -e
 
 ${RED}### Use this if you DO NOT need any logging for these scripts
 ${GREEN}# runs cron_controller.php every minute, if not already running, as user www-data
-* * * * * su www-data -s /bin/bash -c \"/usr/bin/php /var/www/${IPADDY}/include/cron_controller.php\" >/dev/null 2>&1
+* * * * * su www-data -s /bin/bash -c \"/usr/bin/php /var/www/${SITEHTTP}/include/cron_controller.php\" >/dev/null 2>&1
 
 # this can take several minutes to run, especially the first time, so we run it separate
 # runs images_update.php every 30 minutes, if not already running, as user www-data
-*/30 * * * * su www-data -s /bin/bash -c \"/usr/bin/php /var/www/${IPADDY}/include/images_update.php\" >/dev/null 2>&1
+*/30 * * * * su www-data -s /bin/bash -c \"/usr/bin/php /var/www/${SITEHTTP}/include/images_update.php\" >/dev/null 2>&1
 
 ${RED}### Use this if you DO need any logging for these scripts
 ${GREEN}# runs cron_controller.php every minute, if not already running, as user www-data
-* * * * * su www-data -s /bin/bash -c \"/usr/bin/php /var/www/${IPADDY}/include/cron_controller.php\" >> /var/log/nginx/cron_`date +\%Y\%m\%d`.log 2>&1
+* * * * * su www-data -s /bin/bash -c \"/usr/bin/php /var/www/${SITEHTTP}/include/cron_controller.php\" >> /var/log/nginx/cron_`date +\%Y\%m\%d`.log 2>&1
 
 # this can take several minutes to run, especially the first time, so we run it separate
 # runs images_update.php every 30 minutes, if not already running, as user www-data
-*/30 * * * * su www-data -s /bin/bash -c \"/usr/bin/php /var/www/${IPADDY}/include/images_update.php\" >> /var/log/nginx/images_`date +\%Y\%m\%d`.log 2>&1
+*/30 * * * * su www-data -s /bin/bash -c \"/usr/bin/php /var/www/${SITEHTTP}/include/images_update.php\" >> /var/log/nginx/images_`date +\%Y\%m\%d`.log 2>&1
 $CLEAR"
 
